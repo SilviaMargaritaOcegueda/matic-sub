928ced990f296c72c7451765731e320b4398fb8f
Silvia Margarita <83787003+SilviaMargaritaOcegueda@users.noreply.github.com>
2022-10-03 21:42:18 -0600

    Matic transfers subgraph

--- src/matic-token.ts
+++ src/matic-token.ts
@@ -1,74 +1,15 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  MaticToken,
-  Paused,
-  Unpaused,
-  PauserAdded,
-  PauserRemoved,
-  Transfer,
-  Approval
-} from "../generated/MaticToken/MaticToken"
-import { ExampleEntity } from "../generated/schema"
+import { Transfer } from "../generated/MaticToken/MaticToken"
+import { Transfer as MaticTransfer } from "../generated/schema"
 
-export function handlePaused(event: Paused): void {
+export function handleTransfer(event: Transfer): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.account = event.params.account
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.name(...)
-  // - contract.approve(...)
-  // - contract.totalSupply(...)
-  // - contract.transferFrom(...)
-  // - contract.decimals(...)
-  // - contract.increaseAllowance(...)
-  // - contract.isPauser(...)
-  // - contract.paused(...)
-  // - contract.balanceOf(...)
-  // - contract.symbol(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.transfer(...)
-  // - contract.allowance(...)
+  let id = event.transaction.hash
+  let transfer = new MaticTransfer(id)
+  transfer.from = event.params.from
+  transfer.to = event.params.to
+  transfer.amount = event.params.value
+  transfer.timestamp = event.block.timestamp
+  transfer.save()
 }
-
-export function handleUnpaused(event: Unpaused): void {}
-
-export function handlePauserAdded(event: PauserAdded): void {}
-
-export function handlePauserRemoved(event: PauserRemoved): void {}
-
-export function handleTransfer(event: Transfer): void {}
-
-export function handleApproval(event: Approval): void {}
